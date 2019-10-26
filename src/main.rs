3265066eb1a313245c2281fe2cc0e341a40875e2
gathuku <mosesgathuku95@gmail.com>
2019-10-26 13:48:08 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,17 @@ use structopt::StructOpt;
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
 struct Cli {
-    /// The pattern to look for
-    pattern: String,
-    /// The path to the file to read
-    #[structopt(parse(from_os_str))]
-    path: std::path::PathBuf,
+    // /// The pattern to look for
+    // pattern: String,
+    // /// The path to the file to read
+    // #[structopt(parse(from_os_str))]
+    // path: std::path::PathBuf,
+    firstName: String,
+    secondName: String,
 }
 
 
 fn main() {
  let args = Cli::from_args();
+ print("Your name is"+args.firstName);
 }
